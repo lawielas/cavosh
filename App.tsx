6f3ae5b863dd69a5c33a596c5c93e1af43b82595
Lawal Aminu Saleh <lawie.aminu@yahoo.com>
2024-07-30 15:54:12 +0100

    Landing page styled

--- App.tsx
+++ App.tsx
@@ -1,118 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Image, Pressable, SafeAreaView, Text, View} from 'react-native';
+import coffeeCup from './assets/coffee_cup_1.png';
+import coffeeIcon from './assets/coffee_icon.png'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView className="h-screen flex flex-col bg-[#34536A]">
+      <View className="self-end p-2 flex flex-col">
+        <View className="flex gap-2 flex-row items-baseline -top-20">
+          <Image source={coffeeIcon} alt='coffee icon 1' style={{width: 60, height: 60}} className="-rotate-45"  />
+          <Image source={coffeeIcon} alt='coffee icon 2' style={{width: 100, height: 100}} className="opacity-70" />
         </View>
-      </ScrollView>
+        <View className="-mt-24">
+          <Image source={coffeeIcon} alt='coffee icon 2' style={{width: 120, height: 120, resizeMode: 'contain'}} className="rotate-12" />
+        </View>
+      </View>
+      <View className="flex flex-row justify-center -mt-12">
+        <Image source={coffeeCup} alt="coffee cup" style={{width: 350, height: 350}} className="" />
+      </View>
+      <View className="flex items-center mt-2">
+        <Text className="text-2xl text-white">Welcome to</Text>
+        <Text className="text-6xl font-semibold tracking-widest text-[#E65739]">Cavosh</Text>
+      </View>
+      <View className="flex flex-row justify-center mt-3">
+        <Pressable className="bg-[#293440] py-2 px-5 rounded-2xl">
+          <Text className="text-white text-xl">Get Started</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
